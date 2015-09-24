264ebdbba5999e31e977332758066a7c3fe29f31
Gary Sick <garys@cascadianmoonlight.com>
2015-09-24 11:43:17 -0700

    first go at negotiate client

---
+++ lib/win32/sspi/negotiate/client.rb
@@ -0,0 +1,93 @@
+require_relative '../windows/constants'
+require_relative '../windows/structs'
+require_relative '../windows/misc'
+require_relative '../api/client'
+
+module Win32
+  module SSPI
+    module Negotiate
+      class Client
+        include Windows::Constants
+        include Windows::Structs
+        include API::Client
+      
+        attr_reader :spn
+        attr_reader :auth_type
+        attr_reader :context_handle
+        attr_reader :credentials_handle
+      
+        def initialize(spn,options={})
+          @spn = spn
+          @auth_type = "Negotiate"
+        end
+      
+        def acquire_handle
+          return if @credentials_handle
+        
+          @credentials_handle = CredHandle.new
+          expiry = TimeStamp.new
+        
+          status = acquire_credentials_handle(
+            @spn,
+            @auth_type,
+            SECPKG_CRED_OUTBOUND,
+            nil,
+            nil,
+            nil,
+            nil,
+            @credentials_handle,
+            expiry
+          )
+
+          if status != SEC_E_OK
+            @credentials_handle = nil
+            raise SystemCallError.new('AcquireCredentialsHandle', SecurityStatus.new(status))
+          end
+        end
+      
+        def initialize_context(token=nil)
+          ctx = @context_handle
+          context = CtxtHandle.new
+          context_attributes = FFI::MemoryPointer.new(:ulong)
+
+          rflags = ISC_REQ_CONFIDENTIALITY | ISC_REQ_REPLAY_DETECT | ISC_REQ_CONNECTION
+          expiry = TimeStamp.new
+
+          if token
+            input_buffer   = SecBuffer.new.init(token)
+            input_buffer_desc  = SecBufferDesc.new.init(input_buffer)
+          end
+
+          output_buffer = SecBuffer.new.init
+          output_buffer_desc  = SecBufferDesc.new.init(output_buffer)
+        
+          status = initialize_security_context(
+            @credentials_handle,
+            ctx,
+            @spn,
+            rflags,
+            0,
+            SECURITY_NETWORK_DREP,
+            (token ? input_buffer_desc : nil),
+            0,
+            context,
+            output_buffer_desc,
+            context_attributes,
+            expiry
+          )
+
+          if status != SEC_E_OK && status != SEC_I_CONTINUE_NEEDED
+            raise SystemCallError.new('InitializeSecurityContext', SecurityStatus.new(status))
+          else
+            bsize = output_buffer[:cbBuffer]
+            token = output_buffer[:pvBuffer].read_string_length(bsize)
+          end
+        
+          @context_handle = context
+
+          return [status, token]
+        end
+      end
+    end
+  end
+end
---
+++ test/test_win32_sspi_negotiate_client.rb
@@ -0,0 +1,168 @@
+########################################################################
+# Tests for the Win32::SSPI::Negotiate::Client class.
+########################################################################
+require 'test-unit'
+require 'win32/sspi/negotiate/client'
+
+class TC_Win32_SSPI_Negotiate_Client < Test::Unit::TestCase
+  SPN = "HTTP/virtual-pc-serv.bpa.local"
+
+  def setup
+    @client = Win32::SSPI::Negotiate::Client.new(SPN)
+  end
+
+  test "spn basic functionality" do
+    assert_respond_to(@client, :spn)
+    assert_nothing_raised{ @client.spn }
+    assert_kind_of(String, @client.spn)
+    assert_equal "HTTP/virtual-pc-serv.bpa.local", @client.spn
+  end
+
+  test "auth_type basic functionality" do
+    assert_respond_to(@client, :auth_type)
+    assert_nothing_raised{ @client.auth_type }
+    assert_kind_of(String, @client.auth_type)
+    assert_equal "Negotiate", @client.auth_type
+  end
+
+  test "credentials_handle basic functionality" do
+    assert_respond_to(@client, :credentials_handle)
+    assert_nothing_raised{ @client.credentials_handle }
+    assert_nil @client.credentials_handle
+  end
+
+  test "context_handle basic functionality" do
+    assert_respond_to(@client, :context_handle)
+    assert_nothing_raised{ @client.context_handle }
+    assert_nil @client.context_handle
+  end
+
+  test "acquire_handle basic functionality" do
+    assert_respond_to(@client, :acquire_handle)
+    assert_equal 0, @client.method(:acquire_handle).arity
+    assert_respond_to(@client, :acquire_credentials_handle)
+    assert_equal 9, @client.method(:acquire_credentials_handle).arity
+  end
+
+  test "initialize_context basic functionality" do
+    assert_respond_to(@client, :initialize_context)
+    assert_equal -1, @client.method(:initialize_context).arity
+    assert_respond_to(@client, :initialize_security_context)
+    assert_equal 12, @client.method(:initialize_security_context).arity
+  end
+  
+  test "acquire_handle invokes windows api as expected" do
+    client = Class.new(MockNegotiateClient).new(SPN)
+    assert_nothing_raised{ client.acquire_handle }
+    args = client.retrieve_state(:acquire)
+    
+    assert_equal 9, args.length, "acquire_credentials_handle should have 9 arguments"
+    assert_equal SPN, args[0], "unexpected psz_principal"
+    assert_equal 'Negotiate', args[1], "unexpected psz_package"
+    assert_equal Windows::Constants::SECPKG_CRED_OUTBOUND, args[2], "unexpected f_credentialuse"
+    assert_nil args[3], "unexpected pv_logonid"
+    assert_nil args[4], "unexpected p_authdata"
+    assert_nil args[5], "unexpected p_getkeyfn"
+    assert_nil args[6], "unexpected p_getkeyarg"
+    assert_kind_of Windows::Structs::CredHandle, args[7], "unexpected ph_newcredentials"
+    assert_kind_of Windows::Structs::TimeStamp, args[8], "unexpected pts_expiry"
+  end
+  
+  test "acquire_handle raises when windows api returns failed status" do
+    client = Class.new(MockNegotiateClient) do
+      def acquire_credentials_handle(*args)
+        capture_state(:acquire, args)
+        return Windows::Constants::SEC_E_WRONG_PRINCIPAL
+      end
+    end.new(SPN)
+
+    assert_raises(Errno::EINVAL){ client.acquire_handle }
+  end
+  
+  test "initialize_context invokes windows api as expected" do
+    client = Class.new(MockNegotiateClient).new(SPN)
+    assert_nothing_raised{ client.acquire_handle }
+    assert_nothing_raised{ client.initialize_context }
+    args = client.retrieve_state(:isc)
+    
+    assert_equal 12, args.length, "unexpected arguments"
+    assert_kind_of Windows::Structs::CredHandle, args[0], "unexpected ph_credentials"
+    assert_nil args[1], "unexpected ph_context"
+    assert_equal SPN, args[2], "unexpected psz_targetname"
+    
+    rflags = Windows::Constants::ISC_REQ_CONFIDENTIALITY | 
+              Windows::Constants::ISC_REQ_REPLAY_DETECT | 
+              Windows::Constants::ISC_REQ_CONNECTION
+    assert_equal rflags, args[3], "unexpected f_contextreq"
+    assert_equal 0, args[4], "unexpected reserved1"
+    assert_equal Windows::Constants::SECURITY_NETWORK_DREP, args[5], "unexpected targetrep"
+    assert_nil args[6], "unexpected p_input"
+    assert_equal 0, args[7], "unexpected reserved2"
+    assert_kind_of Windows::Structs::CtxtHandle, args[8], "unexpected ph_newcontext"
+    assert_kind_of Windows::Structs::SecBufferDesc, args[9], "unexpected ph_newcontext"
+    assert_kind_of FFI::MemoryPointer, args[10], "unexpected pf_contextattr"
+    assert_kind_of Windows::Structs::TimeStamp, args[11], "unexpected pts_expiry"
+  end
+  
+  test "initialize_context raises when windows api returns failed status" do
+    client = Class.new(MockNegotiateClient) do
+      def initialize_security_context(*args)
+        capture_state(:isc, args)
+        return Windows::Constants::SEC_E_INVALID_TOKEN
+      end
+    end.new(SPN)
+
+    assert_nothing_raised{ client.acquire_handle }
+    assert_raises(Errno::EINVAL){ client.initialize_context }
+  end
+
+  def teardown
+    @client = nil
+  end
+end
+
+class MockNegotiateClient < Win32::SSPI::Negotiate::Client
+  def acquire_credentials_handle(*args)
+    capture_state(:acquire, args)
+    return super
+  end
+  
+  def initialize_security_context(*args)
+    capture_state(:isc, args)
+    return super
+  end
+  
+  def delete_security_context(*args)
+    capture_state(:dsc,args)
+    return super
+  end
+  
+  def free_credentials_handle(*args)
+    capture_state(:fch,args)
+    return super
+  end
+  
+  def capture_state(key,value)
+    self.class.capture_state(key,value)
+  end
+  
+  def retrieve_state(key)
+    self.class.retrieve_state(key)
+  end
+  
+  def self.state
+    @state ||= Hash.new
+  end
+  
+  def self.capture_state(key,value)
+    state[key] = value
+  end
+  
+  def self.retrieve_state(key)
+    state[key]
+  end
+  
+  def self.clear_state
+    state.clear
+  end
+end
