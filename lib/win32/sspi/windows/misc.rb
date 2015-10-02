df3c2bd90808ac713f07a9a68a17ff10e623abe9
Gary Sick <garys@cascadianmoonlight.com>
2015-10-02 11:04:31 -0700

    remove the unused SecurityStatus mapping class

--- lib/win32/sspi/windows/misc.rb
+++ lib/win32/sspi/windows/misc.rb
@@ -10,38 +10,6 @@ class String
   end
 end
 
-# This maps SECURITY_STATUS results into a value that SystemCallError understands
-class SecurityStatus
-  include Windows::Constants
-  
-  def initialize(errno)
-    @new_errno = 0
-    
-    case errno
-      when SEC_E_INSUFFICIENT_MEMORY
-        @new_errno = 12 # ENOMEM
-      when SEC_E_INTERNAL_ERROR
-        @new_errno = 35 # Unknown
-      when SEC_E_INVALID_HANDLE, SEC_E_INVALID_TOKEN, SEC_E_UNSUPPORTED_FUNCTION, SEC_E_WRONG_PRINCIPAL,SEC_E_SECPKG_NOT_FOUND
-        @new_errno = 22 # EINVAL
-      when SEC_E_LOGON_DENIED
-        @new_errno = 1 # EPERM
-      when SEC_E_NO_AUTHENTICATING_AUTHORITY, SEC_E_NO_CREDENTIALS, SEC_E_TARGET_UNKNOWN
-        @new_errno = 3 # ESRCH
-      else
-        @new_errno = 35 # Unknown
-    end
-  end
-  
-  def to_i
-    @new_errno
-  end
-  
-  def to_int
-    @new_errno
-  end
-end
-
 class SecurityStatusError < StandardError
   extend FFI::Library
 
