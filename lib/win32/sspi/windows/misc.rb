54953d95c2ab9524e24e550d1c97abc75e6b0322
Gary Sick <garys@cascadianmoonlight.com>
2015-10-28 13:08:13 -0700

    remove the unused String extension

--- lib/win32/sspi/windows/misc.rb
+++ lib/win32/sspi/windows/misc.rb
@@ -1,15 +1,6 @@
 require 'ffi'
 require_relative 'constants'
 
-class String
-  # Determine if a string is base64 encoded. Use this to automatically
-  # decode tokens if already encoded.
-  #
-  def base64?
-    unpack("m").pack("m").delete("\n") == delete("\n")
-  end
-end
-
 class SecurityStatusError < StandardError
   extend FFI::Library
 
